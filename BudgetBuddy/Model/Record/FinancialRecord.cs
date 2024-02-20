c619a8495d64a554a83f78411fd126949231a77b
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-02-20 14:05:44 +0100

    Add FinancialRecord model

---
+++ BudgetBuddy/Model/Record/FinancialRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace BudgetBuddy.Model.Record;
+
+public class FinancialRecord
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+    public string Name { get; set; }
+    public FinancialRecordTag Tag { get; set; }
+    public RecordType Type { get; set; }
+    public int AccountId { get; set; }
+}
