19236a2af564cdf558146d5b44bfc62774c1e0e5
azabarnahaju <kanikalexandra@gmail.com>
2024-02-20 11:13:59 +0100

    Add User model

---
+++ BudgetBuddy/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetBuddy.Model;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
