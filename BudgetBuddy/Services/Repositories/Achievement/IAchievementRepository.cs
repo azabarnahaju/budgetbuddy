55b71cfda209d9b6ff5fd295c9b2afdbf43c0bd6
azabarnahaju <kanikalexandra@gmail.com>
2024-02-20 16:04:22 +0100

    Add AchievementRepository

---
+++ BudgetBuddy/Services/Repositories/Achievement/IAchievementRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace BudgetBuddy.Services.Repositories.Achievement;
+using BudgetBuddy.Model;
+
+public interface IAchievementRepository
+{
+    IEnumerable<Achievement> GetAllAchievements();
+    Achievement GetAchievement(int id);
+    void AddAchievement(IEnumerable<Achievement> achievements);
+    void AddAchievement(Achievement achievement);
+    void DeleteAchievement(int id);
+    void UpdateAchievement(Achievement achievement);
+}
