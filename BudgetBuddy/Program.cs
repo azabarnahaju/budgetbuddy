e7495c215a51c2f474d44963ff3dca1d0bda3ffa
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-02-20 20:39:46 +0100

    Add FinancialRecordRepository with CRUD

--- BudgetBuddy/Program.cs
+++ BudgetBuddy/Program.cs
@@ -1,3 +1,4 @@
+using BudgetBuddy.Services.Repositories.FinancialRecord;
 using BudgetBuddy.Services.Repositories.User;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddSingleton<IFinancialRecordRepository, FinancialRecordRepository>();
 
 var app = builder.Build();
 
