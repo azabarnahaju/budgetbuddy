2957e2509012a68510b519da3a64530043ff805f
azabarnahaju <kanikalexandra@gmail.com>
2024-02-21 15:22:03 +0100

    Add Equals override method to Achievement model

--- BudgetBuddy/Model/Achievement.cs
+++ BudgetBuddy/Model/Achievement.cs
@@ -5,5 +5,15 @@ public class Achievement
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public IEnumerable<int> Users { get; set; } 
+    public IEnumerable<int> Users { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Achievement || obj is null) return false;
+
+        var other = (Achievement)obj;
+
+        return other.Id == this.Id && other.Name == this.Name && other.Description == this.Description &&
+               other.Users == this.Users;
+    }
 }
