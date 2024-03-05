482296039353ceb0e788e549f3ed337973cc9797
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-03-05 09:19:15 +0100

    add JSON serializer option

--- BudgetBuddy/Program.cs
+++ BudgetBuddy/Program.cs
@@ -13,7 +13,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
