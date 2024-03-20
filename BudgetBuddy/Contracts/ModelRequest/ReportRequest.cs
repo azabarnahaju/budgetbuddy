cb957fea8dda0cf0d40bf901cd83ce3e171d05a8
azabarnahaju <kanikalexandra@gmail.com>
2024-03-20 15:11:32 +0100

    Add ReportRequest model

---
+++ BudgetBuddy/Contracts/ModelRequest/ReportRequest.cs
@@ -0,0 +1,5 @@
+﻿using BudgetBuddy.Model.Enums;
+
+namespace BudgetBuddy.Contracts.ModelRequest;
+
+public record ReportRequest(int AccountId, ReportType ReportType, DateTime? StartDate = null, DateTime? EndDate = null);
---
+++ BudgetBuddy/Contracts/Registration/RegistrationResponse.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetBuddy.Contracts;
+
+public record RegistrationResponse(
+    string Email,
+    string Username);
