dc3871c7269448025d2344563308a910111f1016
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-21 01:51:17 +0100

    Refactor the user interface, add homepage and login logout pages

--- BudgetBuddy/Migrations/20240320180529_initialCreate.Designer.cs
+++ BudgetBuddy/Migrations/20240320180529_initialCreate.Designer.cs
@@ -4,6 +4,7 @@ using BudgetBuddy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BudgetBuddy.Migrations
 {
     [DbContext(typeof(BudgetBuddyContext))]
-    partial class BudgetBuddyContextModelSnapshot : ModelSnapshot
+    [Migration("20240320180529_initialCreate")]
+    partial class initialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
