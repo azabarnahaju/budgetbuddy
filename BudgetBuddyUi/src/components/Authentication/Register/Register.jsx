055152c9ef4d7b3419920b2e684a643dc20d5009
Szilveszter <s.szilveszter1994@gmail.com>
2024-03-21 01:54:59 +0100

    Refactor the login page and the register page in the frontend side

--- BudgetBuddyUi/src/components/Authentication/Register/Register.jsx
+++ BudgetBuddyUi/src/components/Authentication/Register/Register.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { fetchData } from "../../../service/connectionService";
 import SnackBar from "../../Snackbar/Snackbar";
 import Loading from "../../Loading/Loading";
+import InputComponent from "../../FormElements/InputComponent";
+import Navbar from "../../Navbar/Navbar";
+import Footer from "../../Footer/Footer";
+import "./Register.scss";
+import { useNavigate } from "react-router-dom";
+import { SnackbarContext } from "../../../context/snackbarContext";
 
 const registerParams = {
   username: "",
@@ -13,6 +19,8 @@ const registerParams = {
 const Register = () => {
   const [registerInfo, setRegisterInfo] = useState(registerParams);
   const [loading, setLoading] = useState(false);
+  const { setSnackbar } = useContext(SnackbarContext);
+  const navigate = useNavigate();
   const [localSnackbar, setLocalSnackbar] = useState({
     open: false,
     message: "",
@@ -41,11 +49,12 @@ const Register = () => {
         "POST"
       );
       if (response.ok) {
-        setLocalSnackbar({
+        setSnackbar({
           open: true,
           message: response.message,
           type: "success",
         });
+        navigate("/login");
       } else {
         setLocalSnackbar({
           open: true,
@@ -79,99 +88,73 @@ const Register = () => {
   }
 
   return (
-    <div className="vh-100">
+    <div className="vh-100 register-container">
       <SnackBar
         {...localSnackbar}
         setOpen={() => setLocalSnackbar({ ...localSnackbar, open: false })}
       />
-      <div>
-        <div className="text-center">
-          <h4>New Here? Join Us Today!</h4>
-          <h1>Register</h1>
-        </div>
-        <div className="form-container">
-          <form onSubmit={registerUser}>
-            <div className="form-group row">
-            </div>
-            <div className="form-group row">
-              <div className="mb-3">
-                <label className="form-label" htmlFor="registerEmail">
-                  Email
-                </label>
-                <input
-                  className="form-control"
-                  value={registerInfo.email}
-                  name="email"
-                  id="registerEmail"
-                  type="email"
-                  required
-                  onChange={handleSetRegisterInfo}
-                  placeholder="Enter your email"
-                />
+      <Navbar />
+      <div className="register-content">
+        <div className="container mt-5">
+          <div className="text-center">
+            <h4>New Here? Join Us Today!</h4>
+            <h1>Register</h1>
+          </div>
+          <div className="form-container">
+            <form onSubmit={registerUser}>
+              <div className="row d-flex justify-content-center mb-5">
+                <div className="mb-3 col-md-4 border rounded p-4 my-3">
+                  <div className="mb-3">
+                    <InputComponent
+                      text="Email"
+                      name="email"
+                      value={registerInfo.email}
+                      type="email"
+                      onChange={handleSetRegisterInfo}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <InputComponent
+                      text="Username"
+                      name="username"
+                      value={registerInfo.username}
+                      type="text"
+                      onChange={handleSetRegisterInfo}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <InputComponent
+                      text="Password"
+                      name="password"
+                      value={registerInfo.password}
+                      type="password"
+                      onChange={handleSetRegisterInfo}
+                    />
+                  </div>
+                  <div className="mb-5">
+                    <InputComponent
+                      text="Confirm password"
+                      name="confirmPassword"
+                      value={registerInfo.confirmPassword}
+                      type="password"
+                      onChange={handleSetRegisterInfo}
+                    />
+                  </div>
+                  <div className="text-center">
+                    <button
+                      type="submit"
+                      className="btn btn-lg btn-outline-light"
+                    >
+                      Register
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="form-group row">
-              <div className="mb-3">
-                <label className="form-label" htmlFor="registerUsername">
-                  Username
-                </label>
-                <input
-                  className="form-control"
-                  value={registerInfo.username}
-                  name="username"
-                  id="registerUsername"
-                  type="text"
-                  minLength={5}
-                  required
-                  onChange={handleSetRegisterInfo}
-                  placeholder="Enter your username"
-                />
-              </div>
-            </div>
-            <div className="form-group row">
-              <div className="mb-3">
-                <label className="form-label" htmlFor="registerPassword">
-                  Password
-                </label>
-                <input
-                  className="form-control"
-                  value={registerInfo.password}
-                  id="registerPassword"
-                  type="password"
-                  name="password"
-                  minLength={8}
-                  required
-                  onChange={handleSetRegisterInfo}
-                  placeholder="Enter your password"
-                />
-              </div>
-            </div>
-            <div className="form-group row">
-              <div className="mb-5">
-                <label className="form-label" htmlFor="registerConfirmPassword">
-                  Confirm password
-                </label>
-                <input
-                  className="form-control"
-                  value={registerInfo.confirmPassword}
-                  id="registerConfirmPassword"
-                  type="password"
-                  name="confirmPassword"
-                  minLength={8}
-                  required
-                  onChange={handleSetRegisterInfo}
-                  placeholder="Enter your password"
-                />
-              </div>
-            </div>
-            <div className="container d-flex justify-content-center align-items-center">
-              <button type="submit" className="btn btn-lg btn-dark">
-                Register
-              </button>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
---
+++ BudgetBuddyUi/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,93 @@
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/userContext";
+import { logoutUser } from "../../service/authenticationService";
+import { SnackbarContext } from "../../context/snackbarContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInfoCircle,
+  faHouse,
+
+} from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { setSnackbar } = useContext(SnackbarContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const isLoggedOut = await logoutUser();
+    if (isLoggedOut) {
+      setSnackbar({
+        open: true,
+        message: "Successfully logged out.",
+        type: "info",
+      });
+      setCurrentUser(null);
+      navigate("/");
+      return;
+    } else {
+      setSnackbar({
+        open: true,
+        message: "Failed to log out.",
+        type: "info",
+      });
+      navigate("/");
+      return;
+    }
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-container">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <Link className="navbar-brand ms-3 me-3 fs-3" to="/">
+          <img className="logo-img" src="assets/logo.svg" />{" "}
+          <span className="logo-text">BudgetBuddy</span>
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link ms-5 fs-3" to="/">
+                Home <FontAwesomeIcon icon={faHouse} />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link ms-4 fs-3" to="/about">
+                About <FontAwesomeIcon icon={faInfoCircle} />
+              </Link>
+            </li>
+          </ul>
+          <div className="me-2 mb-2">
+            {currentUser ? (
+              <span className="nav-item">
+                <button className="nav-link ms-4 fs-3" onClick={handleLogout}>
+                  Logout
+                </button>
+              </span>
+            ) : (
+              <div className="d-flex nav-item">
+                <Link to="/login" className="fs-3 nav-link">Login</Link>
+                <span className="fs-3 mx-2">or</span>
+                <Link to="/register" className="fs-3 nav-link">Register</Link>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
