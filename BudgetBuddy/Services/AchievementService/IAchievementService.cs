d5162f88a7f3b0b58549720711cd64dc16868a20
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-04-04 11:32:58 +0200

    refactored IAchievementService

--- BudgetBuddy/Services/AchievementService/IAchievementService.cs
+++ BudgetBuddy/Services/AchievementService/IAchievementService.cs
@@ -4,13 +4,7 @@ namespace BudgetBuddy.Services.AchievementService;
 
 public interface IAchievementService
 {
-    Task UpdateAchievements(ApplicationUser user);
-    Task BudgetMasterAchievement(ApplicationUser user, decimal budgetAmount);
-    Task TransactionTrackerAchievement(ApplicationUser user);
-    // Task AccountAchievement(ApplicationUser user);
-    // Task SavingsAchievement(ApplicationUser user);
-    // // Task FirstGoalAchievement(ApplicationUser user);
-    // // Task GoalVisionary(ApplicationUser user);
-    // Task CategorizeAchievement(ApplicationUser user);
-    
+    Task UpdateAccountAchievements(ApplicationUser user);
+    Task UpdateTransactionAchievements(ApplicationUser user);
+    Task UpdateGoalAchievements(ApplicationUser user);
 }
