74ff3d050243b2ce03f74b9d23761263543f1d9d
azabarnahaju <kanikalexandra@gmail.com>
2024-05-02 13:23:50 +0200

    Add new migrations

---
+++ BudgetBuddy/Migrations/20240502100435_UpdateGoal.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetBuddy.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGoal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Goals_Accounts_AccountId",
+                table: "Goals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Goals_AspNetUsers_UserId",
+                table: "Goals");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Goals_Accounts_AccountId",
+                table: "Goals",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Goals_AspNetUsers_UserId",
+                table: "Goals",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Goals_Accounts_AccountId",
+                table: "Goals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Goals_AspNetUsers_UserId",
+                table: "Goals");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Goals_Accounts_AccountId",
+                table: "Goals",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Goals_AspNetUsers_UserId",
+                table: "Goals",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
